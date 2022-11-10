e2e4cef4b0e49901ddea967d49b4e87a2046f15a
Hoc <tapdan1997@gmail.com>
2022-11-10 16:08:39 +0700

    add useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./views/Nav";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Todo from "./views/Todo";
 
 const App = () => {
@@ -11,19 +11,39 @@ const App = () => {
     {
       id: "todo1",
       title: "Watching conan",
+      type: "gaumap",
     },
     {
       id: "todo2",
       title: "Doing homework",
+      type: "gaumap1",
+    },
+    {
+      id: "todo3",
+      title: "Playing game",
+      type: "gaumap",
+    },
+    {
+      id: "todo4",
+      title: "Reading books",
+      type: "gaumap",
     },
   ]);
+
+  useEffect(()=>{
+    console.log('run use effect')
+  }, []);
 
   const handleClick = (event) => {
     if (!address) {
       alert("Empty input");
       return;
     }
-    let newTodo = { id: "", title: address };
+    let newTodo = {
+      id: Math.floor(Math.random() * 100000) + 1,
+      title: address,
+      type: "Quyen",
+    };
     setTodos([...todos, newTodo]);
     setAddress("");
   };
@@ -31,23 +51,34 @@ const App = () => {
     setAddress(event.target.value);
   };
 
+  const deleteDataTodo = (id) => {
+    let currentTodos = todos;
+    currentTodos = currentTodos.filter((item) => item.id !== id);
+    setTodos(currentTodos);
+  };
+
   return (
     <div className="App">
-      <Nav />
       <header className="App-header">
+        <Nav />
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world with React and {name}!</h1>
-        <Todo  
-         todos={todos}
+        <Todo todos={todos} title="All todos" deleteDataTodo={deleteDataTodo} />
+        <Todo
+          todos={todos.filter((item) => item.type === "gaumap")}
+          title={`Gau map todos`}
+          deleteDataTodo={deleteDataTodo}
         />
         <input
           type="text"
           value={address}
           onChange={(event) => handleOnChange(event)}
         ></input>
-        <button type="button" onClick={(event) => handleClick(event)}>
-          Click me
-        </button>
+        <form>
+          <button type="button" onClick={(event) => handleClick(event)}>
+            Click me
+          </button>
+        </form>
       </header>
     </div>
   );
--- src/views/Todo.js
+++ src/views/Todo.js
@@ -1,14 +1,23 @@
 const Todo = (props) => {
-  const todos = props.todos;
+  // const todos = props.todos;
+  const { todos, title, deleteDataTodo } = props;
+  const handleDelete = (id) => {
+    deleteDataTodo(id);
+  };
   return (
     <div className="todo-container">
+      <div className="title">{title}</div>
       {todos.map((todo) => {
         return (
-          <li className="todo-child" key={todo.id}>
-            {todo.title}
-          </li>
+          <div key={todo.id}>
+            <li className="todo-child">
+              {todo.title}
+              &nbsp; &nbsp; <span onClick={() => handleDelete(todo.id)}>x</span>
+            </li>
+          </div>
         );
       })}
+      <hr></hr>
     </div>
   );
 };
