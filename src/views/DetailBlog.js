cd897b81f1039cecf85b9c4c527e4936d2977869
Hoc <tapdan1997@gmail.com>
2022-11-17 17:02:47 +0700

    reusing custom hook

--- src/views/DetailBlog.js
+++ src/views/DetailBlog.js
@@ -1,15 +1,34 @@
 import { useParams, useNavigate } from "react-router-dom";
+import useFetch from "../customize/fetch";
+import "./Blog.scss";
 
 const DetailBlog = () => {
   let { id } = useParams();
+  const {
+    data: dataBlogDetail,
+    isLoading,
+    isError,
+  } = useFetch(`https://jsonplaceholder.typicode.com/posts/${id}`, false);
   let navigate = useNavigate();
   const handleBackData = () => {
-    navigate('/blog');
-  }
+    navigate("/blog");
+  };
   return (
     <>
-      <div><span onClick={handleBackData}>&lt;-- Back</span></div>
-      <h1>Detail Blog {id}</h1>
+      <div>
+        <span onClick={handleBackData}>&lt;-- Back</span>
+        <div className="blog-detail">
+          {dataBlogDetail && (
+            <>
+              <div className="title">
+                Blog ID: {id} -{" "}
+                {isLoading === true ? "Loading..." : dataBlogDetail.title}
+              </div>
+              <div className="content">{dataBlogDetail.body}</div>
+            </>
+          )}
+        </div>
+      </div>
     </>
   );
 };
