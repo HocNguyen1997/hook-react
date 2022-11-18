153a06c9b171fb85fa0b7d043e7b186f58a9ef61
Hoc <tapdan1997@gmail.com>
2022-11-18 21:38:14 +0700

    submit events - validate data

--- src/views/AddNewBlog.js
+++ src/views/AddNewBlog.js
@@ -4,10 +4,20 @@ const AddNewBlog = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const handleSubmit = () => {
+    // event.preventDefault();
+    if (!title) {
+      alert("empty title");
+      return;
+    }
+    if (!content) {
+      alert("empty content");
+      return;
+    }
     console.log("Check data state: ", title, content);
   };
   return (
     <div className="add-new-container">
+      {/* <form onSubmit={handleSubmit}> */}
       <div className="text-add-new">----ADD NEW BLOG----</div>
       <div className="inputs-data">
         <label>Title:</label>
@@ -28,6 +38,10 @@ const AddNewBlog = () => {
       <button className="btn-add-new" onClick={handleSubmit}>
         Submit
       </button>
+      {/* <button className="btn-add-new" type="submit">
+          Submit
+        </button>
+      </form> */}
     </div>
   );
 };
